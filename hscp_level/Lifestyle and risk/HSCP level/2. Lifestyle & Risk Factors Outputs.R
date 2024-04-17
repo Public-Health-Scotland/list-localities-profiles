99ccb1bb581d8d95191f4a76a9b18af6c4b6fbdc
Kennedy Owusu-Afriyie <Kennedy.Owusu-Afriyie@phs.scot>
2024-04-17 13:34:42 +0100

    Add HSCP code to the repository

---
+++ hscp_level/Lifestyle and risk/HSCP level/2. Lifestyle & Risk Factors Outputs.R
@@ -0,0 +1,365 @@
+#############################################################################################.
+#                                                                                           #
+#                   LOCALITY PROFILES LIFESTYLE & RISK FACTORS OUTPUTS CODE                 #
+#                                                                                           #
+#############################################################################################.
+
+## Code used to create infographics, charts, and figures for the Lifestyle & Risk factors  
+#  section of the locality profiles.
+
+## Original script Lucy Dewhurst
+## Original date 10/01/2020
+## October 2022 rewrote parts for smoother process (edits by C.Puech)
+
+############# 1) PACKAGES, DIRECTORY, LOOKUPS, DATA IMPORT + CLEANING #############
+
+##load packages
+library(readxl)
+library(tidyverse)
+library(reshape2)
+library(janitor)
+library(png)
+library(cowplot)
+library(knitr)
+library(gridExtra)
+library(grid)
+library(tidylog)
+library(phsstyles)
+
+#Determine locality (for testing only)
+#LOCALITY <- "Falkirk West"
+
+Sys.umask(mode = "002")
+
+#HSCP <- "Highland"
+
+#Set year of data extracts for folder
+ext_year <- 2022
+
+#Set file path
+lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
+
+#Source in functions code
+source(paste0(lp_path, "Master RMarkdown Document & Render Code/Global Script.R"))
+
+### Geographical lookups and objects ----
+
+#Locality lookup
+lookup <- read_in_localities() 
+
+#Determine HSCP and HB based on Loc
+#HSCP <- as.character(filter(lookup, hscp2019name == HSCP))
+HB <- unique(as.character(filter(lookup, hscp2019name == HSCP)$hb2019name))
+
+
+#Determine other localities based on LOCALITY object
+other_locs <- lookup %>% 
+  select(hscp_locality, hscp2019name) %>% 
+  filter(hscp2019name == HSCP) %>% 
+  arrange(hscp_locality)
+
+#Find number of locs per partnership
+n_loc <- lookup %>% 
+  group_by(hscp2019name) %>% 
+  summarise(locality_n = n()) %>% 
+  filter(hscp2019name == HSCP) %>% 
+  pull(locality_n)
+
+
+
+### Import + clean datasets ----
+
+## Drug-related hospital admissions
+drug_hosp <- readRDS(paste0(lp_path, "Lifestyle & Risk Factors/Data ", ext_year, "/scotpho_data_extract_drug_hosp.RDS"))  %>%
+  clean_scotpho_dat() %>% 
+  mutate(period_short = gsub("to", "-", substr(period, 1, 18)))
+
+check_missing_data_scotpho(drug_hosp)
+
+
+# Alcohol-related hospital admissions
+alcohol_hosp <- readRDS(paste0(lp_path, "Lifestyle & Risk Factors/Data ", ext_year, "/scotpho_data_extract_alcohol_hosp.RDS"))  %>%
+  clean_scotpho_dat() %>% 
+  mutate(period_short = substr(period, 1, 7))
+
+check_missing_data_scotpho(alcohol_hosp)
+
+
+## Alcohol-specific deaths
+alcohol_deaths <- readRDS(paste0(lp_path, "Lifestyle & Risk Factors/Data ", ext_year, "/scotpho_data_extract_alcohol_deaths.RDS"))  %>%
+  clean_scotpho_dat() %>% 
+  mutate(period_short = gsub("to", "-", substr(period, 1, 12)))
+
+check_missing_data_scotpho(alcohol_deaths)
+
+## Bowel screening uptake
+bowel_screening <- readRDS(paste0(lp_path, "Lifestyle & Risk Factors/Data ", ext_year, "/scotpho_data_extract_bowel_screening.RDS"))  %>%
+  clean_scotpho_dat() %>% 
+  mutate(period_short = gsub("to", "-", substr(period, 1, 12)))
+
+check_missing_data_scotpho(bowel_screening)
+
+
+
+############################### 2) OUTPUTS ####################################
+
+
+##### 2a Drug-related hospital admissions #####
+
+##Create variables for latest year
+latest_period_drug_hosp <- unique(filter(drug_hosp, year == max(drug_hosp$year))$period_short)
+earliest_period_drug_hosp <- unique(filter(drug_hosp, year == min(drug_hosp$year))$period_short)
+
+
+##Time trend
+drug_hosp_time_trend <- drug_hosp %>% 
+  scotpho_hscp_time_trend(data = .,
+                     chart_title = "Drug-related Hospital Admissions Time Trend",
+                     xaxis_title = "Financial Year Groups (3-year aggregates)",
+                     yaxis_title = "Drug-related admissions\n(Standardised rates per 100,000)",
+                     string_wrap = 10)
+
+drug_hosp_time_trend
+
+##Bar chart
+drug_hosp_bar <- drug_hosp %>%
+  scotpho_bar_chart_HSCP(data = .,
+                    chart_title = paste0("Drug-related Hospital Admissions by Area, ", max(.$period_short)),
+                    xaxis_title = "Drug-related admissions (Standardised rates per 100,000)")
+
+drug_hosp_bar
+
+
+##Numbers for text
+
+drug_hosp_latest <- unique(filter(drug_hosp, 
+                           year == max(drug_hosp$year) & 
+                             (area_name == HSCP))$measure)
+
+
+drug_hosp_earliest <- unique(filter(drug_hosp, 
+                             (year == min(drug_hosp$year)) & 
+                               (area_name == HSCP))$measure)
+
+drug_hosp_change <- abs((drug_hosp_latest - drug_hosp_earliest)/drug_hosp_earliest*100)
+drug_hosp_change_word <- if_else(drug_hosp_latest > drug_hosp_earliest, 
+                                 "increase", "decrease")
+
+scot_drug_hosp <- filter(drug_hosp, 
+                         year == max(drug_hosp$year) & area_name == "Scotland")$measure
+
+drug_hosp_diff_scot <- if_else(drug_hosp_latest > scot_drug_hosp, "higher", "lower")
+
+
+
+
+##### 2b Alcohol-related hospital admissions #####
+
+##Create variables for latest year
+latest_period_alcohol_hosp <- unique(filter(alcohol_hosp, year == max(alcohol_hosp$year))$period_short)
+earliest_period_alcohol_hosp <- unique(filter(alcohol_hosp, year == min(alcohol_hosp$year))$period_short)
+
+
+##Time trend
+alcohol_hosp_time_trend <- alcohol_hosp %>% 
+  scotpho_hscp_time_trend(data = .,
+                     chart_title = "Alcohol-related Hospital Admissions Time Trend",
+                     xaxis_title = "Financial Year",
+                     yaxis_title = "Alcohol-related admissions\n(Standardised rates per 100,000)",
+                     string_wrap = 10)
+
+alcohol_hosp_time_trend
+
+##Bar chart
+alcohol_hosp_bar <- alcohol_hosp %>%
+  scotpho_bar_chart_HSCP(data = .,
+                    chart_title = paste0("Alcohol-related Hospital Admissions by Area, ", max(.$period_short)),
+                    xaxis_title = "Alcohol-related admissions (Standardised rates per 100,000)")
+
+alcohol_hosp_bar
+
+
+##Numbers for text
+
+alcohol_hosp_latest <- unique(filter(alcohol_hosp, 
+                              year == max(alcohol_hosp$year) & 
+                             (area_name == HSCP))$measure)
+
+alcohol_hosp_earliest <- unique(filter(alcohol_hosp, 
+                            (year == min(alcohol_hosp$year)) & 
+                              (area_name == HSCP))$measure)
+
+alcohol_hosp_change <- abs((alcohol_hosp_latest - alcohol_hosp_earliest)/alcohol_hosp_earliest*100)
+alcohol_hosp_change_word <- if_else(alcohol_hosp_latest > alcohol_hosp_earliest, 
+                                    "increase", "decrease")
+
+scot_alcohol_hosp <- filter(alcohol_hosp, 
+                            year == max(alcohol_hosp$year) & area_name == "Scotland")$measure
+
+alcohol_hosp_diff_scot <- if_else(alcohol_hosp_latest > scot_alcohol_hosp, "higher", "lower")
+
+
+
+##### 2c Alcohol specific deaths #####
+
+##Create variables for latest year
+latest_period_alcohol_deaths <- unique(filter(alcohol_deaths, year == max(alcohol_deaths$year))$period_short)
+earliest_period_alcohol_deaths <- unique(filter(alcohol_deaths, year == min(alcohol_deaths$year))$period_short)
+
+
+##Time trend
+alcohol_deaths_time_trend <- alcohol_deaths %>% 
+  scotpho_hscp_time_trend(data = .,
+                     chart_title = "Alcohol-specific Deaths Time Trend",
+                     xaxis_title = "Year Groups (5-year aggregates)",
+                     yaxis_title = "Alcohol-specific deaths\n(Standardised rates per 100,000)",
+                     string_wrap = 10)
+
+alcohol_deaths_time_trend
+
+##Bar chart
+alcohol_deaths_bar <- alcohol_deaths %>%
+  scotpho_bar_chart_HSCP(data = .,
+                    chart_title = paste0("Alcohol-specific Deaths by Area, ", max(.$period_short)),
+                    xaxis_title = "Alcohol-specific deaths (Standardised rates per 100,000)")
+
+alcohol_deaths_bar
+
+
+##Numbers for text
+
+alcohol_deaths_latest <- unique(filter(alcohol_deaths, 
+                              year == max(alcohol_deaths$year) & 
+                                (area_name == HSCP))$measure)
+
+alcohol_deaths_earliest <- unique(filter(alcohol_deaths, 
+                                (year == min(alcohol_deaths$year)) & 
+                                  (area_name == HSCP))$measure)
+
+alcohol_deaths_change <- abs((alcohol_deaths_latest - alcohol_deaths_earliest)/alcohol_deaths_earliest*100)
+alcohol_deaths_change_word <- if_else(alcohol_deaths_latest > alcohol_deaths_earliest, 
+                                    "higher", "lower")
+
+scot_alcohol_deaths <- filter(alcohol_deaths, 
+                            year == max(alcohol_deaths$year) & area_name == "Scotland")$measure
+
+alcohol_deaths_diff_scot <- if_else(alcohol_deaths_latest > scot_alcohol_deaths, "higher", "lower")
+
+
+
+
+##### 2d Bowel Screening Uptake #####
+
+##Create variables for latest year
+latest_period_bowel_screening <- unique(filter(bowel_screening, year == max(bowel_screening$year))$period_short)
+earliest_period_bowel_screening <- unique(filter(bowel_screening, year == min(bowel_screening$year))$period_short)
+
+
+##Time trend
+bowel_screening_time_trend <- bowel_screening %>% 
+  scotpho_hscp_time_trend(data = .,
+                     chart_title = "Bowel Screening Uptake Time Trend",
+                     xaxis_title = "Year Groups (3-year aggregates)",
+                     yaxis_title = "Bowel screening uptake (%)",
+                     string_wrap = 10)
+
+bowel_screening_time_trend
+
+##Bar chart
+bowel_screening_bar <- bowel_screening %>%
+  scotpho_bar_chart_HSCP(data = .,
+                    chart_title = paste0("Bowel Screening Uptake by Area, ", max(.$period_short)),
+                    xaxis_title = "Bowel screening uptake (%)")
+
+bowel_screening_bar
+
+
+##Numbers for text
+
+bowel_screening_latest <- unique(filter(bowel_screening, 
+                           year == max(bowel_screening$year) & 
+                             (area_name == HSCP))$measure)
+
+bowel_screening_earliest <- unique(filter(bowel_screening,
+                             (year == min(bowel_screening$year)) &
+                               (area_name == HSCP))$measure)
+
+bowel_screening_change <- abs((bowel_screening_latest - bowel_screening_earliest)/bowel_screening_earliest*100)
+bowel_screening_change_word <- if_else(bowel_screening_latest > bowel_screening_earliest,
+                                 "increase", "decrease")
+
+
+scot_bowel_screening <- filter(bowel_screening, 
+                              year == max(bowel_screening$year) & area_name == "Scotland")$measure
+
+bowel_screening_diff_scot <- if_else(bowel_screening_latest > scot_bowel_screening, "higher", "lower")
+
+
+############################### 3) CODE FOR SUMMARY TABLE ###############################
+
+## Make GH objects table for hscp, scot AND other localities in the partnership
+
+#Function to get latest data from scotpho for other localities
+
+other_locs_summary_table <- function(data, latest_year){
+  
+  data %>% 
+    filter(year == latest_year) %>% 
+    filter(area_type == "Locality") %>% 
+    rename("hscp_locality" = "area_name") %>% 
+    right_join(., other_locs) %>% 
+    arrange(hscp_locality) %>%
+    select(hscp_locality, measure) %>% 
+    mutate(measure = round_half_up(measure, 1)) %>%
+    spread(hscp_locality, measure)
+  
+}
+
+# 1. Other locs
+
+other_locs_drug_hosp <- other_locs_summary_table(drug_hosp, 
+                                                latest_year = max(drug_hosp$year)) 
+
+other_locs_alcohol_hosp <- other_locs_summary_table(alcohol_hosp, 
+                                                latest_year = max(alcohol_hosp$year)) 
+
+other_locs_alcohol_deaths <- other_locs_summary_table(alcohol_deaths, 
+                                                  latest_year = max(alcohol_deaths$year)) 
+
+other_locs_bowel_screening <- other_locs_summary_table(bowel_screening, 
+                                             latest_year = max(bowel_screening$year)) 
+
+
+
+#2. HSCP
+
+hscp_drug_hosp <- round_half_up(filter(drug_hosp, year == max(year) & 
+                                        (area_name == HSCP & area_type == "HSCP"))$measure, 1) 
+
+hscp_alcohol_hosp <- round_half_up(filter(alcohol_hosp, year == max(year) & 
+                                        (area_name == HSCP & area_type == "HSCP"))$measure, 1) 
+
+hscp_alcohol_deaths <- round_half_up(filter(alcohol_deaths, year == max(year) & 
+                                          (area_name == HSCP & area_type == "HSCP"))$measure, 1)
+
+hscp_bowel_screening <- round_half_up(filter(bowel_screening, year == max(year) & 
+                             (area_name == HSCP & area_type == "HSCP"))$measure, 1)
+
+
+#3. Scotland
+
+scot_drug_hosp <- round_half_up(scot_drug_hosp, 1)
+
+scot_alcohol_hosp <- round_half_up(scot_alcohol_hosp, 1)
+
+scot_alcohol_deaths <- round_half_up(scot_alcohol_deaths, 1)
+
+scot_bowel_screening <- round_half_up(scot_bowel_screening, 1)
+
+
+
+# detach(package:tidyverse, unload=TRUE)
+# detach(package:reshape2, unload=TRUE)
+# detach(package:janitor, unload=TRUE)
+# detach(package:ggthemes, unload=TRUE)
+detach(package:tidylog, unload=TRUE)
---
+++ hscp_level/Render script/Locality Profiles Render Code_HSCP.R
@@ -0,0 +1,95 @@
+##### LOCALITY PROFILES MASTER DOC RENDER CODE #####
+
+library(readxl)
+library(janitor)
+library(tidyverse)
+library(knitr)
+library(markdown)
+library(rmarkdown)
+
+rm(list = ls())
+
+# system unmask function so files have read-write permissions
+Sys.umask("006")
+
+#Set file path
+lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
+
+#Source in functions code
+source(paste0(lp_path, "Master RMarkdown Document & Render Code/Global Script.R"))
+
+##Specify HSCP here
+## NOTE - make sure that the formatting of the partnership's name matches the lookup  
+HSCP <- "Western Isles"
+
+#Below creates locality list of all the localities in a chosen HSCP
+lookup <- read_in_localities() 
+
+#HSCP_list <- unique(lookup$hscp2019name)
+
+#Create list of localities in chosen HSCP
+#locality_list <- lookup %>% 
+ # filter(hscp2019name == HSCP) %>% 
+ # pull(hscp_locality)
+
+other_locs <- lookup %>% 
+  select(hscp_locality, hscp2019name) %>% 
+  filter(hscp2019name == HSCP) %>% 
+  arrange(hscp_locality)
+
+
+##Loop to create the profiles for all the localities in the list
+
+## There are several stages to the profiles:
+    # 1. Producing the HSCP services map (this takes a while to run so it is produced separately)
+    # 2. Looping through each locality in the HSCP doing the following:
+          #2a. Run each section script for that locality
+          #2b. Run the Rmd for the main body of the profiles
+          #2c. Run the Rmd for the summary tables
+
+# 1. HSCP Services Map
+#source(paste0(lp_path, "Services/Scripts/3. Service HSCP map.R"))
+
+
+# 2. Loop through each locality to create the main body of the profiles and the summary table
+#for (LOCALITY in locality_list){
+  
+  ## 2a) Source in all the scripts for a given LOCALITY
+  
+  #demographics
+  source(paste0(lp_path, "Demographics/Scripts/HSCP level/1. Demographics - Population.R"))
+  source(paste0(lp_path, "Demographics/Scripts/HSCP level/2. Demographics - SIMD.R"))
+  
+  #housing
+  source(paste0(lp_path, "Households/Scripts/HSCP level/Households code.R"))
+  
+  #services
+  source(paste0(lp_path, "Services/Scripts/HSCP level/2. Services data manipulation & table.R"))
+
+  #general health
+  source(paste0(lp_path, "General Health/Scripts/HSCP level/3. General Health Outputs.R"))
+  
+  #lifestyle & risk factors
+  source(paste0(lp_path, "Lifestyle & Risk Factors/Scripts/HSCP level/2. Lifestyle & Risk Factors Outputs.R"))
+  
+  #unscheduled care
+  source(paste0(lp_path, "Unscheduled Care/Scripts/HSCP level/2. Unscheduled Care outputs.R"))
+  
+  #appendices
+  source(paste0(lp_path, "Master RMarkdown Document & Render Code/Tables for Appendix.R"))
+  
+  # Remove tidylog package which messes up outputs
+  detach(package:tidylog, unload=TRUE)
+
+  ## 2b) Create the main body of the profiles
+  rmarkdown::render(paste0(lp_path, "Master RMarkdown Document & Render Code/HSCP level/Locality_Profiles_Master_Markdown_HSCP.Rmd"),
+                    output_file =  paste0(HSCP, " - Locality Profile.docx"),
+                    output_dir = paste0(lp_path, "Master RMarkdown Document & Render Code/Output/"))
+  
+  ## 2c) Create the summary tables
+  #rmarkdown::render(paste0(lp_path, "Summary Table/Summary-Table-Markdown - HSCP.Rmd"),
+                   # output_file = paste0(HSCP, " - Summary Table.docx"),
+                   # output_dir = paste0(lp_path, "Master RMarkdown Document & Render Code/Output/Summary Tables/"))
+#}
+
+ 
