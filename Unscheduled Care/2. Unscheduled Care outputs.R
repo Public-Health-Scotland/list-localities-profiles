ecb40a3709dd5acb7ae68ebb1adba6b7a5acd53e
James McMahon <james.mcmahon@phs.scot>
2024-05-20 10:45:27 +0100

    Fix issue where `{data.table}` was masking `{lubridate}`

--- Unscheduled Care/2. Unscheduled Care outputs.R
+++ Unscheduled Care/2. Unscheduled Care outputs.R
@@ -41,7 +41,7 @@ library(fst)
 # Set date limit for financial year
 # Unless we're in Q4 use the previous FY as the max
 max_fy <- ifelse(
-  quarter(Sys.Date(), fiscal_start = 4) != 4,
+  lubridate ::quarter(Sys.Date(), fiscal_start = 4) != 4,
   phsmethods::extract_fin_year(Sys.Date() - years(1)),
   phsmethods::extract_fin_year(Sys.Date())
 )
