6b2022f7ae120eb9b1bf52a04e01fc696736d0ae
Moohan <Moohan@users.noreply.github.com>
2024-05-21 16:00:20 +0000

    Style code (GHA)

--- Unscheduled Care/2. Unscheduled Care outputs.R
+++ Unscheduled Care/2. Unscheduled Care outputs.R
@@ -38,7 +38,7 @@ library(reshape2)
 # Set date limit for financial year
 # Unless we're in Q4 use the previous FY as the max
 max_fy <- ifelse(
-  lubridate ::quarter(Sys.Date(), fiscal_start = 4) != 4,
+  lubridate::quarter(Sys.Date(), fiscal_start = 4) != 4,
   phsmethods::extract_fin_year(Sys.Date() - years(1)),
   phsmethods::extract_fin_year(Sys.Date())
 )
