c76df7f0e40a6a365f1918cd0a4dd9da32141793
JosieSteele <josie.steele@phs.scot>
2024-06-18 14:29:51 +0100

    Adjusting Lookup

--- make_custom_lookup.R
+++ make_custom_lookup.R
@@ -5,37 +5,21 @@ source("Master RMarkdown Document & Render Code/Global Script.R")
 # Read in the Excel Lookup
 custom_lookup <- readxl::read_excel(fs::path(
   lp_path,
-  "custom_lookups/forres_and_lossiemouth_april_2022.xlsx"
-)) |>
-  # Clean names
-  clean_names() |>
-  # Make all rows work
-  mutate(
-    sub_area = if_else(sub_area == "Not yet required", "Other areas", sub_area)
-  )
+  "custom_lookups/custom_south_ayrshire_localities.xlsx"
+))
 
 # Make locality lookup
 custom_lookup |>
-  # Make variables match the locality lookup
-  mutate(
-    datazone2011 = datazone,
-    hscp_locality = sub_area,
-    hscp2019name = "Moray",
-    hscp2019 = "S37000019",
-    hb2019name = "NHS Grampian",
-    hb2019 = "S08000020",
-    .keep = "none"
-  ) |>
   bind_rows(
     read_in_localities(dz_level = TRUE) |>
-      filter(hscp2019name != "Moray")
+      filter(hscp2019name != "South Ayrshire")
   ) |>
   mutate(hscp_locality = sub("&", "and", hscp_locality, fixed = TRUE)) |>
   # Write out as a parquet file
   arrow::write_parquet(
     fs::path(
       lp_path,
-      "custom_lookups/forres_and_lossiemouth_april_2022.parquet"
+      "custom_lookups/custom_south_ayrshire_localities.parquet"
     ),
     version = "latest",
     compression = "zstd"
@@ -44,15 +28,15 @@ custom_lookup |>
 # Make IZ lookup
 custom_lookup |>
   distinct(
-    intzone2011 = intermediate_zone,
-    intzone2011name = iz_name,
-    hscp_locality = sub_area
+    intzone2011,
+    intzone2011name,
+    hscp_locality
   ) |>
   # Write out as a parquet file
   arrow::write_parquet(
     fs::path(
       lp_path,
-      "custom_lookups/forres_and_lossiemouth_iz_april_2022.parquet"
+      "custom_lookups/custom_south_ayrshire_localities_iz.parquet"
     ),
     version = "latest",
     compression = "zstd"
