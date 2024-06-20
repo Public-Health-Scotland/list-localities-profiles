4abfd4233677982df0b91d184ff53d535115b4fb
There717 <79308287+There717@users.noreply.github.com>
2024-06-20 11:50:59 +0100

    Update Master RMarkdown Document & Render Code/Locality Profiles Render Code.R
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- Master RMarkdown Document & Render Code/Locality Profiles Render Code.R
+++ Master RMarkdown Document & Render Code/Locality Profiles Render Code.R
@@ -64,7 +64,6 @@ for (LOCALITY in locality_list) {
   # services
   source("./Services/2. Services data manipulation & table.R")
   source("./Services/3. Service HSCP map.R")
-#plot(service_map)
   # general health
   source("./General Health/3. General Health Outputs.R")
 
