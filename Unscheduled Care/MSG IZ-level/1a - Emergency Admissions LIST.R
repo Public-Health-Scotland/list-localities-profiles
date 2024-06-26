b210bfe2f9f48907bb36c830baf85df8d1afaccf
JosieSteele <josie.steele@phs.scot>
2024-06-26 17:42:32 +0100

    Filtering to make quicker to run

--- Unscheduled Care/MSG IZ-level/1a - Emergency Admissions LIST.R
+++ Unscheduled Care/MSG IZ-level/1a - Emergency Admissions LIST.R
@@ -30,7 +30,7 @@ smr01_extract <- as_tibble(dbGetQuery(channel, statement = "SELECT LINK_NO, ADMI
                                       SIGNIFICANT_FACILITY, ADMISSION_TYPE, DR_POSTCODE,
                                       AGE_IN_YEARS, HBTREAT_CURRENTDATE, ADMISSION,
                                       DISCHARGE, URI,  INTZONE_2011  FROM ANALYSIS.SMR01_PI
-                                      WHERE DISCHARGE_DATE >= TO_DATE('2017-04-01','YYYY-MM-DD')")) %>% # AND COUNCIL_AREA_2019 = 'S12000035'"
+                                      WHERE DISCHARGE_DATE >= TO_DATE('2017-04-01','YYYY-MM-DD') AND HBRES_CURRENTDATE = 'S08000015'")) %>% # AND COUNCIL_AREA_2019 = 'S12000035'"
   # tidy up variable names
   clean_names()
 
