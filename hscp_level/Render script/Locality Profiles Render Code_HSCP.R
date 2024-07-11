5fc13163bf28bdfac27851fba670a7d4cf609ec6
James McMahon <james.mcmahon@phs.scot>
2024-07-11 13:16:47 +0100

    Update some paths and style

--- hscp_level/Render script/Locality Profiles Render Code_HSCP.R
+++ hscp_level/Render script/Locality Profiles Render Code_HSCP.R
@@ -14,7 +14,7 @@ Sys.umask("006")
 # Set file path
 lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
 
-#Source in functions code
+# Source in functions code
 source("Master RMarkdown Document & Render Code/Global Script.R")
 
 ## Specify HSCP here
@@ -26,70 +26,49 @@ lookup <- read_in_localities()
 
 # HSCP_list <- unique(lookup$hscp2019name)
 
-# Create list of localities in chosen HSCP
-# locality_list <- lookup %>%
-# filter(hscp2019name == HSCP) %>%
-# pull(hscp_locality)
-
 other_locs <- lookup %>%
   select(hscp_locality, hscp2019name) %>%
   filter(hscp2019name == HSCP) %>%
   arrange(hscp_locality)
 
 
-## Loop to create the profiles for all the localities in the list
+# demographics
+source("hscp_level/Demographics/1. Demographics - Population.R")
+source("hscp_level/Demographics/2. Demographics - SIMD.R")
 
-## There are several stages to the profiles:
-# 1. Producing the HSCP services map (this takes a while to run so it is produced separately)
-# 2. Looping through each locality in the HSCP doing the following:
-# 2a. Run each section script for that locality
-# 2b. Run the Rmd for the main body of the profiles
-# 2c. Run the Rmd for the summary tables
+# housing
+source("hscp_level/Households/HSCP level/Households code.R")
 
-# 1. HSCP Services Map
-# source(paste0(lp_path, "Services/Scripts/3. Service HSCP map.R"))
+# services
+source("hscp_level/Services/3. Service HSCP map.R") # Map
+# source(paste0(lp_path, "Services/Scripts/HSCP level/2. Services data manipulation & table.R"))
+# source("hscp_level/Services/") # Nothing in this folder
 
+# general health
+source("hscp_level/General Health/3. General Health Outputs.R")
 
-# 2. Loop through each locality to create the main body of the profiles and the summary table
-#for (LOCALITY in locality_list){
-  
-  ## 2a) Source in all the scripts for a given LOCALITY
-  
-  #demographics
-  source("hscp_level/Demographics/1. Demographics - Population.R")
-  source("hscp_level/Demographics/2. Demographics - SIMD.R")
-  
-  #housing
-  source("hscp_level/Households/HSCP level/Households code.R")
-  
-  #services
-  source(paste0(lp_path, "Services/Scripts/HSCP level/2. Services data manipulation & table.R"))
-  source("hscp_level/Services/") # Nothing in this folder
+# lifestyle & risk factors
+source("hscp_level/Lifestyle and risk/HSCP level/2. Lifestyle & Risk Factors Outputs.R")
 
-  #general health
-  source("hscp_level/General Health/3. General Health Outputs.R")
+# unscheduled care
+source("hscp_level/unschedule care/2. Unscheduled Care outputs.R")
 
-  #lifestyle & risk factors
-  source("hscp_level/Lifestyle and risk/HSCP level/2. Lifestyle & Risk Factors Outputs.R")
-  
-  #unscheduled care
-  source("hscp_level/unschedule care/2. Unscheduled Care outputs.R")
+# appendices
+source("Master RMarkdown Document & Render Code/Tables for Appendix.R")
 
-  #appendices
-  source("Master RMarkdown Document & Render Code/Tables for Appendix.R")
+# Remove tidylog package which messes up outputs
+detach(package:tidylog, unload = TRUE)
 
-  # Remove tidylog package which messes up outputs
-  detach(package:tidylog, unload=TRUE)
+## 2b) Create the main body of the profiles
+rmarkdown::render(
+  input = "hscp_level/Render script/Locality_Profiles_Master_Markdown_HSCP.Rmd",
+  output_file = paste0(HSCP, " - Locality Profile.docx"),
+  output_dir = paste0(lp_path, "Master RMarkdown Document & Render Code/Output/")
+)
 
-  ## 2b) Create the main body of the profiles
-  rmarkdown::render(paste0(lp_path, "Master RMarkdown Document & Render Code/HSCP level/Locality_Profiles_Master_Markdown_HSCP.Rmd"),
-                    output_file =  paste0(HSCP, " - Locality Profile.docx"),
-                    output_dir = paste0(lp_path, "Master RMarkdown Document & Render Code/Output/"))
-  
-  ## 2c) Create the summary tables
-  #rmarkdown::render(paste0(lp_path, "Summary Table/Summary-Table-Markdown - HSCP.Rmd"),
-                   # output_file = paste0(HSCP, " - Summary Table.docx"),
-                   # output_dir = paste0(lp_path, "Master RMarkdown Document & Render Code/Output/Summary Tables/"))
-#}
-
- 
+## 2c) Create the summary tables
+rmarkdown::render(
+  input = "hscp_level/Summary-Table-Markdown - HSCP.Rmd",
+  output_file = paste0(HSCP, " - Summary Table.docx"),
+  output_dir = paste0(lp_path, "Master RMarkdown Document & Render Code/Output/Summary Tables/")
+)
