f26665495d56f758e0dea75571bddfcd30705a7e
JosieSteele <josie.steele@phs.scot>
2024-10-02 11:21:28 +0100

    changes to lookup and custom function

--- make_custom_lookup.R
+++ make_custom_lookup.R
@@ -1,27 +1,30 @@
 lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
 
 source("Master RMarkdown Document & Render Code/Global Script.R")
+dz_lookup <- read_in_localities(T) |>
+  clean_names() |>
+  #filter(hscp2019name == "East Ayrshire") |>
+  select(datazone2011,hscp_locality,hb2019name)
+  
 
 # Read in the Excel Lookup
 custom_lookup <- readxl::read_excel(fs::path(
   lp_path,
-  "custom_lookups/custom_south_ayrshire_localities.xlsx"
+  "custom_lookups/Ayrshire_mmw.xlsx"
 ))|>
   # Clean names
-  clean_names()
+  clean_names() |>
+  left_join(dz_lookup) 
+
 
 # Make locality lookup
 custom_lookup |>
-  bind_rows(
-    read_in_localities(dz_level = TRUE) |>
-      filter(hscp2019name != "South Ayrshire")
-  ) |>
   mutate(hscp_locality = sub("&", "and", hscp_locality, fixed = TRUE)) |>
   # Write out as a parquet file
   arrow::write_parquet(
     fs::path(
       lp_path,
-      "custom_lookups/custom_south_ayrshire_localities.parquet"
+      "custom_lookups/custom_mmw.parquet"
     ),
     version = "latest",
     compression = "zstd"
@@ -32,13 +35,13 @@ custom_lookup |>
   distinct(
     intzone2011,
     intzone2011name,
-    hscp_locality
+    mm_ward_name
   ) |>
   # Write out as a parquet file
   arrow::write_parquet(
     fs::path(
       lp_path,
-      "custom_lookups/custom_south_ayrshire_localities_iz.parquet"
+      "custom_lookups/custom_mmw_iz.parquet"
     ),
     version = "latest",
     compression = "zstd"
