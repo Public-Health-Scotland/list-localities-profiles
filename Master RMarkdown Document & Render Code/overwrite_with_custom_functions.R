fb22633daf2651f37989416e027cca215953c354
JosieSteele <josie.steele@phs.scot>
2024-10-08 16:51:51 +0100

    changing the custom function back

--- Master RMarkdown Document & Render Code/overwrite_with_custom_functions.R
+++ Master RMarkdown Document & Render Code/overwrite_with_custom_functions.R
@@ -5,12 +5,12 @@ read_in_localities <- function(dz_level = FALSE) {
       "custom_lookups/custom_mmw.parquet"
     )
   )
-
+  
   if (!dz_level) {
     data <- data |>
-      dplyr::distinct(hscp_locality, hscp2019name, hscp2019, hb2019name, hb2019,mm_ward_name,mm_ward_code)
+      dplyr::distinct(hscp_locality, hscp2019name, hscp2019, hb2019name, hb2019)
   }
-
+  
   return(data)
 }
 
@@ -18,7 +18,7 @@ read_in_iz <- function(dz_all = FALSE) {
   iz_lookup <- arrow::read_parquet(
     fs::path(lp_path, "custom_lookups/custom_mmw_iz.parquet")
   )
-
+  
   if (dz_all) {
     all_iz <- readr::read_csv(
       file = fs::path(
@@ -33,7 +33,7 @@ read_in_iz <- function(dz_all = FALSE) {
       ),
       lazy = TRUE
     )
-
+    
     iz_lookup <- iz_lookup |>
       dplyr::full_join(
         all_iz,
@@ -48,14 +48,13 @@ read_in_iz <- function(dz_all = FALSE) {
         .keep = "unused"
       ) |> 
       dplyr::mutate(
-        intzone2011 = dplyr::if_else(is.na(intzone2011.y), intzone2011.x, intzone2011.y),
-        .keep = "unused")|> 
-      dplyr::mutate(
         intzone2011name = dplyr::if_else(is.na(intzone2011name.y), intzone2011name.x, intzone2011name.y),
-        .keep = "unused")|> 
+        .keep = "unused"
+      ) |> 
       dplyr::mutate(
-        mm_ward_name = dplyr::if_else(is.na(mm_ward_name.y), mm_ward_name.x, mm_ward_name.y),
-        .keep = "unused")
+        intzone2011 = dplyr::if_else(is.na(intzone2011.y), intzone2011.x, intzone2011.y),
+        .keep = "unused"
+      ) 
   }
   
   return(iz_lookup)
@@ -66,7 +65,7 @@ read_in_iz <- function(dz_all = FALSE) {
 summarise_iz_to_locality <- function(data, iz_lookup = read_in_iz(dz_all = FALSE)) {
   iz_data <- data |>
     inner_join(iz_lookup, by = c("area_code" = "intzone2011"))
-
+  
   if (anyNA(iz_data$numerator)) {
     locality_data <- iz_data |>
       group_by(indicator, year, period, area_name = hscp_locality, definition, data_source) |>
@@ -84,7 +83,7 @@ summarise_iz_to_locality <- function(data, iz_lookup = read_in_iz(dz_all = FALSE
         denominator = sum(numerator / measure * 100000), # Adjust for per 100,000
         measure = sum(weighted_measure) / sum(numerator) * 100000, # Adjust for per 100,000
         se = sqrt(sum(numerator * (measure - lower_confidence_interval)^2 / (denominator - 1)) +
-          sum(numerator * (upper_confidence_interval - measure)^2 / (denominator - 1))),
+                    sum(numerator * (upper_confidence_interval - measure)^2 / (denominator - 1))),
         lower_confidence_interval = measure - 1.96 * se,
         upper_confidence_interval = measure + 1.96 * se,
         area_type = "HSC locality",
@@ -92,11 +91,11 @@ summarise_iz_to_locality <- function(data, iz_lookup = read_in_iz(dz_all = FALSE
       ) |>
       select(!denominator, !se)
   }
-
+  
   new_data <- data |>
     filter(area_type %in% c("HSC locality","Scotland","HSC partnership","Health board")) |>
     bind_rows(locality_data)
-
+  
   return(new_data)
 }
 
@@ -111,4 +110,3 @@ clean_scotpho_dat <- function(data) {
       area_type = if_else(area_type == "HSC locality", "Locality", area_type)
     )
 }
-
