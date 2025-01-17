ba95dc146b3cb69241144dd18e6566bba9c993de
RyanDuffy01 <82060940+RyanDuffy01@users.noreply.github.com>
2025-01-17 17:33:58 +0000

    Style code

--- Services/2. Services data manipulation & table.R
+++ Services/2. Services data manipulation & table.R
@@ -29,13 +29,13 @@ library(data.table)
 ext_year <- 2024
 
 ## Set Locality (for testing only)
-#LOCALITY <- "Falkirk West"
+# LOCALITY <- "Falkirk West"
 
 ## Set file path
-#lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
+# lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
 
 # Source in functions code
-#source("Master RMarkdown Document & Render Code/Global Script.R")
+# source("Master RMarkdown Document & Render Code/Global Script.R")
 
 
 ### Geographical lookups and objects ----
@@ -175,4 +175,3 @@ services_tibble <- tibble(
     nrow(other_care_type)
   )
 )
-
