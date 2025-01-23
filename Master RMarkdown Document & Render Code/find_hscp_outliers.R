21bb1fe206b27e3b969046af39d1ef10f3bc95df
Moohan <5982260+Moohan@users.noreply.github.com>
2025-01-23 17:04:18 +0000

    Style code

--- Master RMarkdown Document & Render Code/find_hscp_outliers.R
+++ Master RMarkdown Document & Render Code/find_hscp_outliers.R
@@ -1,13 +1,14 @@
-# Add some metrics to the lookup 
-lookup_with_metrics <- lookup |> 
+# Add some metrics to the lookup
+lookup_with_metrics <- lookup |>
   mutate(
     locality_length = str_width(hscp_locality),
-    hscp_length = str_width(hscp2019name)) |> 
-  group_by(hscp2019name) |> 
+    hscp_length = str_width(hscp2019name)
+  ) |>
+  group_by(hscp2019name) |>
   mutate(
     n_localities = n_distinct(hscp_locality),
     total_length = str_width(paste0(paste(hscp_locality, collapse = ""), hscp2019name))
-    ) |> 
+  ) |>
   ungroup()
 
 # Use the metrics to take the HSCP with:
@@ -24,8 +25,8 @@ outlier_hscps <- bind_rows(
   slice_min(lookup_with_metrics, locality_length, with_ties = FALSE),
   slice_min(lookup_with_metrics, n_localities, with_ties = FALSE),
   slice_min(lookup_with_metrics, total_length, with_ties = FALSE),
-) |> 
-  pull(hscp2019name) |> 
+) |>
+  pull(hscp2019name) |>
   unique()
 
 rm(lookup_with_metrics)
