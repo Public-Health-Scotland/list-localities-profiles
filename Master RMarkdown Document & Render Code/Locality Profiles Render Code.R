cac5b6b763db26cf96f86c2f69700e24d9798d69
Andrew-Saul <andrew.saul@phs.scot>
2025-03-05 13:21:08 +0000

    changed lp_path

--- Master RMarkdown Document & Render Code/Locality Profiles Render Code.R
+++ Master RMarkdown Document & Render Code/Locality Profiles Render Code.R
@@ -12,7 +12,7 @@ Sys.umask("006")
 source("Master RMarkdown Document & Render Code/Global Script.R")
 
 # Set file path
-lp_path <- lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
+lp_path <- lp_path <- "/conf/LIST_analytics/West Dunbartonshire/Locality Profiles Combined/"
 output_dir <- path(lp_path, "Master RMarkdown Document & Render Code", "Output")
 
 # Below creates locality list of all the localities in a chosen HSCP
@@ -24,7 +24,7 @@ lookup <- read_in_localities()
 # For a larger test, use the below to produce profiles for HSCPs likely to cause issues.
 # source("Master RMarkdown Document & Render Code/find_hscp_outliers.R")
 # hscp_list <- outlier_hscps
-hscp_list <- "Angus"
+hscp_list <- "West Dunbartonshire"
 
 # NOTE - This checks that it exactly matches the lookup
 stopifnot(all(hscp_list %in% unique(lookup[["hscp2019name"]])))
@@ -36,7 +36,7 @@ for (HSCP in hscp_list) {
   locality_list <- lookup |>
     filter(hscp2019name == HSCP) |>
     pull(hscp_locality)
-
+}
   # Loop to create the profiles for all the localities in the list
 
   # There are several stages to the profiles:
@@ -45,10 +45,10 @@ for (HSCP in hscp_list) {
   # 1b. Run the Rmd for the main body of the profiles
   # 1c. Run the Rmd for the summary tables
 
-  loop_env <- c(ls(), "loop_env")
+ # loop_env <- c(ls(), "loop_env")
 
   # 1. Loop through each locality to create the main body of the profiles and the summary table
-  for (LOCALITY in locality_list) {
+#  for (LOCALITY in locality_list) {
     # 1a) Source in all the scripts for a given LOCALITY
 
     # Demographics ----
@@ -90,8 +90,8 @@ for (HSCP in hscp_list) {
 
     # End of loop housekeeping ----
     # Clean up the environment by restoring it to the 'pre-loop' state.
-    rm(list = setdiff(ls(), loop_env))
+ #   rm(list = setdiff(ls(), loop_env))
     # Force garbage collection to free up memory
     gc()
-  }
-}
+ # }
+#}
