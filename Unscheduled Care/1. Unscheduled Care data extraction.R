7d8d9b0d358de18132761f218a99937d5071515e
James McMahon <james.mcmahon@phs.scot>
2025-03-31 13:50:59 +0100

    Update Unscheduled Care/1. Unscheduled Care data extraction.R

--- Unscheduled Care/1. Unscheduled Care data extraction.R
+++ Unscheduled Care/1. Unscheduled Care data extraction.R
@@ -15,7 +15,7 @@ ext_year <- 2024
 latest_msg_folder <- "2024-12 December"
 
 # Set locality profiles file path
-lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
+# lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
 
 ## Packages
 library(tidyverse)
