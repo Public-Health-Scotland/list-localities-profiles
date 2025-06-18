9bf87cefadd5beb85f102e492788576decdefcc8
James McMahon <james.mcmahon@phs.scot>
2025-06-18 16:49:16 +0000

    Remove trailing commas from functions
    These are functions which take any number of unnamed arguments and a comma has been left in after the last argument.

--- Unscheduled Care/2. Unscheduled Care outputs.R
+++ Unscheduled Care/2. Unscheduled Care outputs.R
@@ -1135,7 +1135,7 @@ first_fy_rate_ae_areas <- filter(
   ae_att_areas,
   financial_year == min(financial_year),
   location == LOCALITY,
-  area_type == "Locality",
+  area_type == "Locality"
 )$data
 
 latest_ae_att_loc <- ae_att_areas %>%
