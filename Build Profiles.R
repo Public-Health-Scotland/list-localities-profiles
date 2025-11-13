9f6897a1af6ee34a0281184631650315ff38e5d4
Ryan Duffy <ryan.duffy@phs.scot>
2025-11-13 13:45:42 +0000

    add cover page, sensitivity labels, table formatting function, testing chapter

--- Build Profiles.R
+++ Build Profiles.R
@@ -10,8 +10,7 @@ Sys.umask("006")
 source("Master RMarkdown Document & Render Code/Global Script.R")
 
 # Set file path
-# Set file path
-lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
+lp_path <- lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
 output_dir <- path(lp_path, "Profiles Output")
 
 # Below creates locality list of all the localities in a chosen HSCP
@@ -23,7 +22,7 @@ lookup <- read_in_localities()
 # For a larger test, use the below to produce profiles for HSCPs likely to cause issues.
 # source("Master RMarkdown Document & Render Code/find_hscp_outliers.R")
 # hscp_list <- outlier_hscps
-hscp_list <- "Angus"
+hscp_list <- "Falkirk"
 
 # NOTE - This checks that it exactly matches the lookup
 stopifnot(all(hscp_list %in% unique(lookup[["hscp2019name"]])))
@@ -35,41 +34,41 @@ for (HSCP in hscp_list) {
   locality_list <- lookup |>
     filter(hscp2019name == HSCP) |>
     pull(hscp_locality)
-  
+
   # Loop to create the profiles for all the localities in the list
-  
+
   # There are several stages to the profiles:
   # 1. Looping through each locality in the HSCP doing the following:
   # 1a. Run each section script for that locality
   # 1b. Run the Rmd for the main body of the profiles
   # 1c. Run the Rmd for the summary tables
-  
+
   loop_env <- c(ls(), "loop_env")
-  
+
   # 1. Loop through each locality to create the main body of the profiles and the summary table
   for (LOCALITY in locality_list) {
     # 1a) Source in all the scripts for a given LOCALITY
-    
+
     # Demographics ----
     source("Demographics/1. Demographics - Population.R")
     source("Demographics/2. Demographics - SIMD.R")
-    
+
     # Housing ----
     source("Households/Households Code.R")
-    
+
     # Services ----
     source("Services/2. Services data manipulation & table.R")
     source("Services/3. Service HSCP map.R")
-    
+
     # General Health ----
     source("General Health/3. General Health Outputs.R")
-    
+
     # Lifestyle & Risk Factors ----
     source("Lifestyle & Risk Factors/2. Lifestyle & Risk Factors Outputs.R")
-    
+
     # Unscheduled Care ----
     source("Unscheduled Care/2. Unscheduled Care outputs.R")
-    
+
     # Appendices ----
     source("Master RMarkdown Document & Render Code/Tables for Appendix.R")
     
@@ -82,11 +81,34 @@ for (HSCP in hscp_list) {
       output_format = "bookdown::word_document2",
       config_file = "_bookdown.yaml"
     )
+
+    document_path <- paste0(output_dir, glue("/{LOCALITY} - Locality Profile.docx"))
+    
+    cover_page_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/Master RMarkdown Document & Render Code/phs-mngtinfo-cover.docx"
+    
+    main_title <- "Locality Profile"
+    subtitle <- "test"
+    render_date <- format(Sys.Date(),"%d/%m/%Y")
+    
+    add_cover_page(
+      
+      document_path,
+      cover_page_path,
+      main_title,
+      subtitle,
+      render_date
+      
+    )
+    
+    phstemplates::apply_sensitivity_label(document_path,"OFFICIAL")
     
     # End of loop housekeeping ----
     # Clean up the environment by restoring it to the 'pre-loop' state.
     rm(list = setdiff(ls(), loop_env))
     # Force garbage collection to free up memory
     gc()
+    
+    
+    
   }
 }
