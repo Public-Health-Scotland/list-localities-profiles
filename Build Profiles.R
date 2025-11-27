4d942643d23e41ee6d913033ec5df82d10a47ad9
James Hayes <james.hayes2@phs.scot>
2025-11-27 15:53:56 +0000

    Create bookdown files

--- Build Profiles.R
+++ Build Profiles.R
@@ -1,6 +1,5 @@
-##### LOCALITY PROFILES MASTER DOC RENDER CODE #####
-
-library(rmarkdown)
+library(knitr)
+library(bookdown)
 
 rm(list = ls())
 
@@ -11,7 +10,7 @@ Sys.umask("006")
 source("Master RMarkdown Document & Render Code/Global Script.R")
 
 # Set file path
-lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
+lp_path <- lp_path <- "/conf/LIST_analytics/West Hub/02 - Scaled Up Work/RMarkdown/Locality Profiles/"
 output_dir <- path(lp_path, "Master RMarkdown Document & Render Code", "Output")
 
 # Below creates locality list of all the localities in a chosen HSCP
@@ -26,7 +25,7 @@ lookup <- read_in_localities()
 hscp_list <- "Angus"
 
 # NOTE - This checks that it exactly matches the lookup
-stopifnot(hscp_list %in% unique(lookup[["hscp2019name"]]))
+stopifnot(all(hscp_list %in% unique(lookup[["hscp2019name"]])))
 
 # Loop over HSCP ----
 # 'looping' over one HSCP is fine.
@@ -35,58 +34,52 @@ for (HSCP in hscp_list) {
   locality_list <- lookup |>
     filter(hscp2019name == HSCP) |>
     pull(hscp_locality)
-
+  
   # Loop to create the profiles for all the localities in the list
-
+  
   # There are several stages to the profiles:
   # 1. Looping through each locality in the HSCP doing the following:
   # 1a. Run each section script for that locality
   # 1b. Run the Rmd for the main body of the profiles
   # 1c. Run the Rmd for the summary tables
-
+  
   loop_env <- c(ls(), "loop_env")
-
+  
   # 1. Loop through each locality to create the main body of the profiles and the summary table
   for (LOCALITY in locality_list) {
     # 1a) Source in all the scripts for a given LOCALITY
-
+    
     # Demographics ----
     source("Demographics/1. Demographics - Population.R")
     source("Demographics/2. Demographics - SIMD.R")
-
+    
     # Housing ----
     source("Households/Households Code.R")
-
+    
     # Services ----
     source("Services/2. Services data manipulation & table.R")
     source("Services/3. Service HSCP map.R")
-
+    
     # General Health ----
     source("General Health/3. General Health Outputs.R")
-
+    
     # Lifestyle & Risk Factors ----
     source("Lifestyle & Risk Factors/2. Lifestyle & Risk Factors Outputs.R")
-
+    
     # Unscheduled Care ----
     source("Unscheduled Care/2. Unscheduled Care outputs.R")
-
+    
     # Appendices ----
     source("Master RMarkdown Document & Render Code/Tables for Appendix.R")
-
-    # Render main profile content ----
-    render(
-      input = "Master RMarkdown Document & Render Code/Locality_Profiles_Master_Markdown.Rmd",
+    
+    # Make sure your working directory is the project root
+    bookdown::render_book(
+      input = "lp_bookdown",
+      output_dir = output_dir,
       output_file = glue("{LOCALITY} - Locality Profile.docx"),
-      output_dir = output_dir
+      output_format = "bookdown::word_document2"
     )
-
-    # Render the summary table(s) ----
-    render(
-      input = "Summary Table/Summary-Table-Markdown.Rmd",
-      output_file = glue("{LOCALITY} - Summary Table.docx"),
-      output_dir = path(output_dir, "Summary Tables")
-    )
-
+    
     # End of loop housekeeping ----
     # Clean up the environment by restoring it to the 'pre-loop' state.
     rm(list = setdiff(ls(), loop_env))
