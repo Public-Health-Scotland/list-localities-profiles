Public-Health-Scotland/list-localities-profiles

commit 6f4d11e20ce69cb280fa119987e05adcc522b8b1

Make an empty subtitle the default

Master RMarkdown Document & Render Code/Global Script.R
@@ -716,7 +716,7 @@ add_cover_page <- function(
   document_path,
   cover_page_path,
   main_title,
-  subtitle,
+  subtitle = "",
   date
 ) {
   # Load and update cover page

Build Profiles.R
@@ -103,14 +103,12 @@ for (HSCP in hscp_list) {
       "phs-mngtinfo-cover.docx"
     )
 
-    subtitle <- ""
     render_date <- format(Sys.Date(), "%d/%m/%Y")
 
     add_cover_page(
       document_path,
       cover_page_path,
       main_title,
-      subtitle,
       render_date
     )
 

commit 2d7c54f6db37b721f48f297450c6751894c4e787

Fix bug with `add_cover_page` that I introduced in 6f4d11e2

Master RMarkdown Document & Render Code/Global Script.R
@@ -716,8 +716,8 @@ add_cover_page <- function(
   document_path,
   cover_page_path,
   main_title,
-  subtitle = "",
-  date = format(Sys.Date(), "%d/%m/%Y")
+  date = format(Sys.Date(), "%d/%m/%Y"),
+  subtitle = ""
 ) {
   # Load and update cover page
   cover_page <- cover_page_path |>