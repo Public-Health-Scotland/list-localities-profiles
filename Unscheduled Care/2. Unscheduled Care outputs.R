4f8e3376f7dca637c925c644e4725904b3bda5e1
RyanDuffy01 <82060940+RyanDuffy01@users.noreply.github.com>
2026-07-17 09:09:19 +0000

    Style code

--- Unscheduled Care/2. Unscheduled Care outputs.R
+++ Unscheduled Care/2. Unscheduled Care outputs.R
@@ -36,28 +36,27 @@ max_year <- 2024
 ########################## SECTION 2: Lookups & Populations ###############################
 
 aggregate_area_data <- function(data, measure) {
-  
   data %>%
-    
+
     # Group Up To Locality Level + Combine Sexes ----
-  
-  summarise(
-    {{ measure }} := sum({{ measure }}),
-    .by = c(
-      "year",
-      "financial_year",
-      "hb2019name",
-      "hscp2019name",
-      "hscp_locality",
-      "age_group"
-    )
-  ) %>%
+
+    summarise(
+      {{ measure }} := sum({{ measure }}),
+      .by = c(
+        "year",
+        "financial_year",
+        "hb2019name",
+        "hscp2019name",
+        "hscp_locality",
+        "age_group"
+      )
+    ) %>%
     mutate(location = hscp_locality) %>%
     mutate(level = "Locality") %>%
-    
+
     bind_rows(
       summarise(
-        filter(.,level == "Locality"),
+        filter(., level == "Locality"),
         {{ measure }} := sum({{ measure }}),
         .by = c(
           "year",
@@ -70,20 +69,20 @@ aggregate_area_data <- function(data, measure) {
         mutate(location = hscp2019name) %>%
         mutate(level = "HSCP")
     ) %>%
-    
+
     bind_rows(
       summarise(
-        filter(.,level == "Locality"),
+        filter(., level == "Locality"),
         {{ measure }} := sum({{ measure }}),
         .by = c("year", "financial_year", "hb2019name", "age_group")
       ) %>%
         mutate(location = hb2019name) %>%
         mutate(level = "HB")
     ) %>%
-    
+
     bind_rows(
       summarise(
-        filter(.,level == "Locality"),
+        filter(., level == "Locality"),
         {{ measure }} := sum({{ measure }}),
         .by = c("year", "financial_year", "age_group")
       ) %>%
@@ -107,7 +106,6 @@ aggregate_area_data <- function(data, measure) {
         levels = c("Locality", "HSCP", "HB", "Scotland")
       )
     )
-  
 }
 
 
