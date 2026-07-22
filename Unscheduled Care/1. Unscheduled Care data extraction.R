233f94de074fbda4613ba39b094bbf3130aa8dc4
Ryan Duffy <ryan.duffy@phs.scot>
2026-07-22 10:43:58 +0000

    use derived partinship instead of council so that localities and partnerships align

--- Unscheduled Care/1. Unscheduled Care data extraction.R
+++ Unscheduled Care/1. Unscheduled Care data extraction.R
@@ -172,7 +172,7 @@ msg_dd <- msg_dd_raw %>%
   mutate(financial_year = phsmethods::extract_fin_year(month)) %>%
   mutate(hscp_locality = gsub("&", "and", locality, fixed = TRUE)) %>%
   # this data set has some data with partnership but no locality, need to tidy names
-  mutate(hscp2019name = gsub("&", "and", council, fixed = TRUE)) %>%
+  mutate(hscp2019name = gsub("&", "and", derived_partnership, fixed = TRUE)) %>%
   mutate(hscp2019name = ptsp(hscp2019name)) %>%
   group_by(
     financial_year,
